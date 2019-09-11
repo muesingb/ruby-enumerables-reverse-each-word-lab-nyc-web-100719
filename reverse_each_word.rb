a531b0fca15777940a9d173b9883a2354c1bc04c
Barbara Muesing <muesingb@gmail.com>
2019-09-11 21:35:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word (sentence)
-array = sentence.split
-new_array = array.collect do |word| word.reverse end
+new_array = sentence.split.collect do |word| word.reverse end
 return new_array.join(" ")
 end
